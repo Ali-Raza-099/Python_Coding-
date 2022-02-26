bfeb83f9b0a4cbafde23273f2a2b55eb5090cca6
Mati ul Rehman <iammatiz00&gmail.com>
2022-02-26 16:09:00 +0500

    data types in Python;

---
+++ Data_Types_And_Printing_It/data_Types_And_Printing_It.py
@@ -0,0 +1,55 @@
+#printing data types
+
+#integer
+a=12
+print(a)
+
+#float
+
+b=12.23
+print(b)
+
+#String
+c="Github is best"
+print(c)
+
+#char 
+d='Matiz loves Python'
+print(d)
+
+#Accessing values in Strings
+
+university="Comsats University Islamabad"
+print(university[3]) #now it will print s as index starts from C.
+
+#Integration of Strings
+
+firstName="John"
+secondName="Elia"
+print(firstName,secondName)
+
+#String Concatenatin
+
+demo="South"
+print(demo+"ampton")
+
+#deleting Strings
+
+let="Expelliarmuse"
+del let
+#print(let) -->it will generate the error that let is not defined
+
+#Repetition of strings
+
+alpha="Matiz"
+print(alpha*4)
+
+#In usage
+
+beta="Broken"
+print('o' in beta)
+
+#String formatting
+
+print("My name is %s and my age is %d"%("Matiz",21))
+
