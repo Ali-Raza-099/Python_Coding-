7bacd550a2c7548eacfa65e4bee59a25e651ffb2
Mati ul Rehman <iammatiz00&gmail.com>
2022-02-26 17:19:22 +0500

    Tuples are added;

---
+++ Tuples/Tuples.py
@@ -0,0 +1,15 @@
+#Tuples in Python
+tuples=(
+        "Name","Matiz",
+        "Class","SP20-BSE",
+        "Reg","SP20-BSE-042",
+        "Age",21,
+        "Hobby","Coding"
+    )
+print(tuples[2])
+
+print(tuples)
+
+#deleting tuples
+
+del tuples
