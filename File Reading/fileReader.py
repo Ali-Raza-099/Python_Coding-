9de6d3d028ad9babf28ae34b1eb0445ba11723db
Mati ul Rehman <iammatiz00&gmail.com>
2022-03-01 15:09:26 +0500

    file handling added;

---
+++ File Reading/fileReader.py
@@ -0,0 +1,22 @@
+#Open a file  and read it
+fileOpen= open("Pak.txt","r+")
+str=fileOpen.read()
+print(str)
+fileOpen.close()
+
+
+#Open a file and apend it
+
+fileOpen= open("Pak.txt","a+")
+fileOpen.write("\nIts hot today")
+fileOpen.close()
+
+fileOpen= open("Pak.txt","r+")
+str=fileOpen.read()
+print(str)
+fileOpen.close()
+
+fileOpen=open("Pak.txt","r+")
+place=fileOpen.tell()
+print("Position:",place)
+
